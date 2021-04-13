9ed5ea705f09f77723c2798d5e6cf4ff86beacbf
Truong Cao Xuan <54384725+truongcaoxuan@users.noreply.github.com>
2021-04-13 10:17:56 +0700

    Update getdb.sh

--- addfile/getdb.sh
+++ addfile/getdb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-fileid="1auBOPyiooV-0FrOc7GIAAMonFpMXjZMP"
+#fileid="1auBOPyiooV-0FrOc7GIAAMonFpMXjZMP"
+fileid="1O_vA-_AgYpgpN5JVBrhLsFiN6LajLYzF"
 filename="db.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
