6773ce1f96c057ff520af4d177e304c7d7d822b8
Truong Cao Xuan <54384725+truongcaoxuan@users.noreply.github.com>
2021-04-13 10:22:04 +0700

    Update getdb.sh

--- addfile/getdb.sh
+++ addfile/getdb.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+if [ $# != 2 ]; then
+   echo "Usage: downG.sh ID save_name"
+   exit 0
+fi
 #fileid="1auBOPyiooV-0FrOc7GIAAMonFpMXjZMP"
-fileid="1O_vA-_AgYpgpN5JVBrhLsFiN6LajLYzF"
-filename="db.zip"
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+#$fileid="1O_vA-_AgYpgpN5JVBrhLsFiN6LajLYzF"
+#$filename="db.zip"
+
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=$1" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=$1" -o $2
