837e4633d20e7eeb4271861e2ad67f052fd5b4a3
Truong Cao Xuan <54384725+truongcaoxuan@users.noreply.github.com>
2021-04-19 09:20:28 +0700

    Update getDataDrive.sh

--- addfile/getDataDrive.sh
+++ addfile/getDataDrive.sh
@@ -3,9 +3,13 @@ if [ $# != 2 ]; then
    echo "Usage: getDataDrive.sh ID save_name"
    exit 0
 fi
-#fileid="1auBOPyiooV-0FrOc7GIAAMonFpMXjZMP"
-#$fileid="1O_vA-_AgYpgpN5JVBrhLsFiN6LajLYzF"
+#-- db epoch 258
+#fileid="1O_vA-_AgYpgpN5JVBrhLsFiN6LajLYzF"
 #$filename="db.zip"
-
+#
+#-- bin 1.26.2 
+#$fileid="1y1ZBVXGInMJ8F2X8wVQ47ZvIgY8CEFFi"
+#$filename="bin.zip"
+#
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=$1" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=$1" -o $2
